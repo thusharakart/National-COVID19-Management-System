5150ee5eb90d6103e158eab10133d610ec9794b6
thusharakart <thusharakart@gmail.com>
2021-06-30 23:39:51 +0530

    refactor(security): removed spring security temporaty;

--- Backend Implementation/ncms/src/main/java/com/example/ncms/configurations/PasswordGenerator.java
+++ Backend Implementation/ncms/src/main/java/com/example/ncms/configurations/PasswordGenerator.java
@@ -1,15 +1,15 @@
-package com.example.ncms.configurations;
-
-/*To use this we need spring security dependency*/
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class PasswordGenerator {
-
-    public static void main(String[] args){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "akash";
-        String encodedPassword = encoder.encode(rawPassword);
-
-        System.out.println("Encoded password : " + encodedPassword);
-    }
-}
+//package com.example.ncms.configurations;
+//
+///*To use this we need spring security dependency*/
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//public class PasswordGenerator {
+//
+//    public static void main(String[] args){
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        String rawPassword = "akash";
+//        String encodedPassword = encoder.encode(rawPassword);
+//
+//        System.out.println("Encoded password : " + encodedPassword);
+//    }
+//}
