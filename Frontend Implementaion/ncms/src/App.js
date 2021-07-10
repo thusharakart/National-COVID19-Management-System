d7ad2ecff1ffe855b3f43739fe10f0bbfcef2d74
thusharakart <thusharakart@gmail.com>
2021-07-10 20:19:24 +0530

    feat(routers): add react router dom to app;

--- Frontend Implementaion/ncms/src/App.js
+++ Frontend Implementaion/ncms/src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
+
 import Home from "./views/home/pages/Home";
+import Login from "./views/login/pages/Login";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
 	return (
-		<div className="App">
-			<Home />
-		</div>
+		<React.Fragment>
+			<Router>
+				<Switch>
+					<Route path="/home" component={Home}></Route>
+					<Route path="/" component={Login}></Route>
+				</Switch>
+			</Router>
+			<CssBaseline />
+		</React.Fragment>
 	);
 }
 
